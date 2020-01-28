0713a2bd51f0f9e6934015710e7bd02d5cc4786c
Ben Taylor <me@taybenlor.com>
2020-01-28 14:52:48 +1100

    Fixes per code review

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 set -ue -o pipefail -o errtrace
 
 DEPLOY_PATH=${DEPLOY_HASH:-local}
 
-node node_modules/pxt-core/built/pxt.js staticpkg -r "$DEPLOY_HASH"
+node node_modules/pxt-core/built/pxt.js staticpkg -r "$DEPLOY_PATH"
 cp built_hexfiles/* built/packaged/$DEPLOY_PATH/hexcache/
 cd built/packaged/$DEPLOY_PATH/
 ln -s docs/static
